a910ae342bc1d4ccae4f506aeea60eda76997a0c
Vaibhav Kumar <vaibhav2k1507@gmail.com>
2022-09-04 14:01:00 +0530

    UPDATED

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const activePage = window.location.pathname;
+const activePage = window.location.pathname[0];
 
 const navLinks = document.querySelectorAll('nav a');
 
 navLinks.forEach((link) => {
-  if (link.href.includes(`${activePage}`)) {
+  if (activePage === '/' && link.href.includes('index.html')) {
+    link.classList.add('active');
+  } else if (link.href.includes(`${activePage}`)) {
     link.classList.add('active');
   }
 });
