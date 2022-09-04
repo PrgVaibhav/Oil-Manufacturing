0deae79cb0cde08c07f52d9ec8b77b5c0545efa5
Vaibhav Kumar <vaibhav2k1507@gmail.com>
2022-09-04 14:35:29 +0530

    UPDATED

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const activePage = window.location.pathname[0];
+let activePage = window.location.pathname;
+
+// const subDirectory = activePage.replace(/\.tld\/([^/]+)\//, '');
+
+const subDirectory = '/Oil-Manufacturing/';
+activePage = activePage.replace(subDirectory, '/');
 
 const navLinks = document.querySelectorAll('nav a');
 
 navLinks.forEach((link) => {
-  if (activePage === '/' && link.href.includes('index.html')) {
-    link.classList.add('active');
+  if (activePage === '/') {
+    if (link.href.includes('index.html')) {
+      link.classList.add('active');
+    }
   } else if (link.href.includes(`${activePage}`)) {
     link.classList.add('active');
   }
